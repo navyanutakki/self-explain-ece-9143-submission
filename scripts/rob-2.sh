8c88e62c05797c60b66021a051cf14008b0234f8
Gauri Gupta <gaurigupta.315@gmail.com>
2023-12-22 14:47:28 -0800

    scripts for roberta testing

---
+++ scripts/rob-2.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --job-name=rob-2
+#SBATCH --output=%x.out
+#SBATCH --mem=64GB
+#SBATCH --time=00:30:00
+#SBATCH --gres=gpu:2
+#SBATCH --cpus-per-task=4
+
+
+singularity exec --nv --overlay /scratch/gg2751/hproject/overlay-15GB-500K.ext3:ro \
+    /scratch/work/public/singularity/cuda11.6.124-cudnn8.4.0.27-devel-ubuntu20.04.4.sif /bin/bash -c "source /ext3/env.sh; \
+    python run-model.py"
